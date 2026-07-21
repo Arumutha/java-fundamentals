94d89c393fb6f81cabd1136b33a708044e3943c0
Arumutha <arumuthadevi@gmail.com>
2026-07-21 20:23:50 +0530

    Add files via upload

---
+++ 02.Input-Output/sum.java
@@ -0,0 +1,11 @@
+//Read two numbers and print their sum.
+
+public class sum{
+    public static void main(String[] args) {
+       int a=10;
+       int b=20;
+       int sum=a+b;
+       System.out.println("The sum is :"+sum);
+
+    }
+}
